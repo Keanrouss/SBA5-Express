15b92f8f88861b5eb82a3448cc2a769c5242ed7e
Keanu Rouss <timajori1@gmail.com>
2025-06-15 16:13:17 -0400

    second commit

---
+++ data/Users.js
@@ -0,0 +1,20 @@
+const Users = [
+   {
+    id: 1,
+    name: "Uryuko",
+    username: "Ury2021",
+    email: "ury2021@gmail.com",
+   },
+   { id: 2,
+    name: "Kaiya",
+    username: "Kaiya2023",
+    email: "kaiya2023@gmail.com",
+   },
+   { id: 3,
+    name: "Hajime",
+    username: "Hajime2021",
+    email: "Hajime2021@gmail.com",
+   },
+];
+
+module.export = Users
