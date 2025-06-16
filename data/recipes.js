ca3c82327fa5a74110d9e4a6087f3df3af494e72
Keanu Rouss <timajori1@gmail.com>
2025-06-15 23:55:52 -0400

    6th

---
+++ data/recipes.js
@@ -0,0 +1,28 @@
+export const Recipes =[
+    {
+    id: 1,
+    title: 'Sauteed Potatoes with Parmasean Cheese',
+    category: 'Main Entree',
+    instructions: 'For potatoes: Cut potatoes in slices, Add oil in pan, cook potatoes for 5 mins then add into a baking bowl. Sprinkle parmesan and bake for 35mins at 380 ',
+
+
+  },
+  {
+    id: 2,
+    title: 'Spicy Jerk Chicken',
+    category: 'Main Entree',
+    instructions: 'Clean Chicken with lime water. Make slits in chicken. Add 3 spoonfull of WalkersWoods seasoning. Rub into chicken very well. Fry chicken for 3 mins each side. Add in baking pan and bake for 45mins at 385 degree.',
+  },
+{
+    id: 3,
+    title: 'Passion Fruit Lemonade',
+    category: 'Beverage',
+    instructions:'Cut passion fruit, add fruit onto a straightener, add sugar and pour water and keep moving fruits back and forth until while adding more water. Once only seeds are left. Discard seeds and add more water to match prefer taste. Add some lemon juice to add lemonade flavor. Add Ice and serve cold.',
+  },
+{
+ id: 4,
+    title: 'Bagel with Peanut Butter',
+    category: 'Snacks',
+    instructions:'Toast bagel for 1 min in toaster. Add peanut butter and spread onto bagel',
+}
+]
